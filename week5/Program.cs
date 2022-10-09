4e29a195ad98bb19b7a1f7bac66fcc6b2df88570
Wouter van der Wekken <Wouter.van.der.Wekken@gmail.com>
2022-10-09 13:16:05 +0200

    Multithreaded socket serving

--- week5/Program.cs
+++ week5/Program.cs
@@ -1,14 +1,31 @@
 ﻿using System.Net.Sockets;
+using System.Threading;
 
 namespace Pretpark
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             TcpListener server = new TcpListener(new System.Net.IPAddress(new byte[] { 127,0,0,1 }), 5000);
             server.Start();
-            using Socket connectie = server.AcceptSocket();
+            int connectionCount = 0;
+
+            while(true)
+            {
+                Console.WriteLine(" >> [Main] Opening socket");
+                Socket connectie = server.AcceptSocket();
+                connectionCount++;
+                Console.WriteLine(" >> [Main] Calling new Thread for client " + connectionCount + "...");
+                new Thread(() => ThreadProc(connectie, connectionCount)).Start();
+                //Console.WriteLine(" >> [Main] ");
+            }
+        }
+
+        private static void ThreadProc(object obj, int connectionCount)
+        {
+            Console.WriteLine(" >> [Connection Thread " + connectionCount + "] Connection accepted");
+            var connectie = (Socket)obj;
             using Stream request = new NetworkStream(connectie);
             using StreamReader requestLezer = new StreamReader(request);
             string[]? regel1 = requestLezer.ReadLine()?.Split(" ");
@@ -28,7 +45,9 @@ namespace Pretpark
                 char[] bytes = new char[(int)contentLength];
                 requestLezer.Read(bytes, 0, (int)contentLength);
             }
+            Thread.Sleep(5000);
             connectie.Send(System.Text.Encoding.ASCII.GetBytes("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 11\r\n\r\nHello World"));
+            Console.WriteLine(" >> [Connection Thread " + connectionCount + "] Response sent");
         }
     }
 }
