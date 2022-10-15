fb637abc041f6c900822cc155039b144b9a11cff
Wouter van der Wekken <Wouter.van.der.Wekken@gmail.com>
2022-10-15 17:08:07 +0200

    DB migration & seed of 'Geslacht'

--- week6/Data/PretparkContext.cs
+++ week6/Data/PretparkContext.cs
@@ -12,5 +12,15 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Geslacht>().HasData(new Geslacht(1, "Man"));
+            modelBuilder.Entity<Geslacht>().HasData(new Geslacht(2, "Vrouw"));
+            modelBuilder.Entity<Geslacht>().HasData(new Geslacht(3, "Geheim"));
+            modelBuilder.Entity<Geslacht>().HasData(new Geslacht(4, "Anders"));
+        }
+
         public DbSet<Attractie> Attractie { get; set; } = default!;
+        public DbSet<GebruikerMetGeslacht> GebruikerMetGeslacht {get; set;} = default!;
     }
