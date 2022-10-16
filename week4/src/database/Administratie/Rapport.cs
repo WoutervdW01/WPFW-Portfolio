9b5e95dc3cafcfb1bd20dbb90e323ee31efc9e91
Wouter van der Wekken <Wouter.van.der.Wekken@gmail.com>
2022-10-16 15:40:59 +0200

    Start on DemografischRapport; Non-working code

---
+++ week4/src/database/Administratie/Rapport.cs
@@ -0,0 +1,16 @@
+namespace pretpark.database.administratie;
+
+abstract class Rapport
+{
+    public abstract string Naam();
+    public abstract Task<string> Genereer();
+    public async Task VoerUit() => await File.WriteAllTextAsync(Naam() + ".txt", await Genereer());
+    public async Task VoerPeriodiekUit(Func<bool> stop)
+    {
+        while (!stop())
+        {
+            await VoerUit();
+            await Task.Delay(1000);
+        }
+    }
+}
